958024aa7d5efa06e5d99ac7f7b1a998d51f3c93
luisxfgc <lipegbi01@gmail.com>
2022-09-30 17:57:32 -0300

    create login UI

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
 import * as React from 'react'
-import { View, Text, Button } from 'react-native'
+import { TouchableOpacity } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { auth } from './src/config/firebase'
 import Login from './src/views/Login'
 import Dashboard from './src/views/Dashboard'
 import Register from './src/views/Register'
-import { auth } from './src/config/firebase'
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Bem-vindo</Text>
-    </View>
-  )
-}
+import PasswordRecovery from './src/views/PasswordRecovery'
 
 const Stack = createNativeStackNavigator()
 
@@ -21,11 +14,6 @@ function App({ navigation }) {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerShown: false }}
-        />
         <Stack.Screen
           name="Login"
           component={Login}
@@ -36,13 +24,14 @@ function App({ navigation }) {
           component={Register}
           options={{ headerShown: false }}
         />
+        <Stack.Screen name="PasswordRecovery" component={PasswordRecovery} />
         <Stack.Screen
           name="Dashboard"
           component={Dashboard}
           options={() => {
             return {
               headerRight: () => (
-                <Button
+                <TouchableOpacity
                   type="clear"
                   title="Sair"
                   onPress={() => {
--- src/views/Login.js
+++ src/views/Login.js
@@ -1,58 +1,100 @@
 import React, { useState } from 'react'
 import { auth } from '../config/firebase'
 import {
+  Alert,
+  Image,
+  Pressable,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
+  ActivityIndicator,
   View,
 } from 'react-native'
 
 export default function Login({ navigation }) {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
 
   const handleSubmit = () => {
     auth
       .signInWithEmailAndPassword(email, password)
       .then((userCredential) => {
         // Signed in
-        let user = userCredential.user
+        setIsLoading(true)
+        var user = userCredential.user
         navigation.navigate('Dashboard')
+        console.log(user.email)
         // ...
       })
       .catch((error) => {
+        setIsLoading(false)
+        if (email === '' || password === '') {
+          Alert.alert(
+            'Ocorreu uma falha ao tentar realizar o login',
+            'Email ou senha estão vazios' +
+              '\n' +
+              '\n' +
+              'Erro completo:' +
+              '\n' +
+              error.message
+          )
+        }
         console.log(error.message)
       })
+      .finally(setIsLoading(true))
   }
   return (
     <View style={styles.container}>
+      <Image
+        source={require('../assets/login_image.png')}
+        style={styles.headerImage}
+      />
       <View style={styles.subcontainer}>
-        <View>
-          <Text style={styles.title}>Fazer Login</Text>
+        <Text style={styles.title}>Fazer Login no helpUS</Text>
+        <Text style={styles.subtitle}>
+          Preencha seus dados para entre ou cria sua conta caso ainda não tenha.
+        </Text>
+        <View style={styles.form}>
+          <TextInput
+            style={styles.input}
+            placeholder="Endereço de email"
+            value={email}
+            autoCapitalize="none"
+            onChangeText={(email) => setEmail(email)}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Senha"
+            value={password}
+            secureTextEntry={true}
+            onChangeText={(password) => setPassword(password)}
+          />
+          <Pressable onPress={() => navigation.navigate('PasswordRecovery')}>
+            <Text
+              style={{ paddingVertical: 10, color: '#4C4D4F', opacity: 0.6 }}
+            >
+              Esqueci minha senha
+            </Text>
+          </Pressable>
         </View>
-        <TextInput
-          placeholder="Endereço de email"
-          value={email}
-          autoCapitalize="none"
-          onChangeText={(email) => setEmail(email)}
-        />
-        <TextInput
-          placeholder="Senha"
-          value={password}
-          secureTextEntry={true}
-          onChangeText={(password) => setPassword(password)}
-        />
-        <TouchableOpacity style={styles.button} onPress={handleSubmit}>
-          <Text>Entrar</Text>
+      </View>
+      <View style={styles.containerButton}>
+        <TouchableOpacity style={styles.buttonLogin} onPress={handleSubmit}>
+          {isLoading ? (
+            <ActivityIndicator size={25} color="white" />
+          ) : (
+            <Text style={{ color: '#eeeeee', fontSize: 18 }}>Entrar</Text>
+          )}
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.buttonRegister}
+          onPress={() => navigation.push('Register')}
+        >
+          <Text style={{ color: '#eeeeee', fontSize: 18 }}>Criar conta</Text>
         </TouchableOpacity>
       </View>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate('Register')}
-      >
-        <Text>Criar conta</Text>
-      </TouchableOpacity>
     </View>
   )
 }
@@ -60,17 +102,69 @@ export default function Login({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginHorizontal: 20,
-  },
-  title: {
-    fontSize: 20,
-    textAlign: 'center',
-    fontWeight: 'bold',
+    marginVertical: '15%',
+    justifyContent: 'space-between',
+    alignItems: 'baseline',
+    marginHorizontal: 32,
   },
   subcontainer: {
-    margin: 50,
+    width: '100%',
+    paddingVertical: 10,
     justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  headerImage: {
+    width: '60%',
+    height: '25%',
+    alignSelf: 'center',
+  },
+  form: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  containerButton: {
+    width: '100%',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    paddingVertical: 10,
+    color: '#2E2E2E',
+  },
+  subtitle: {
+    fontSize: 18,
+    marginBottom: 25,
+    color: '#4C4D4F',
+    opacity: 0.7,
+  },
+  input: {
+    width: '100%',
+    fontSize: 14,
+    padding: 15,
+    borderRadius: 10,
+    marginVertical: 10,
+    backgroundColor: '#f6f6f6',
+  },
+  buttonLogin: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 15,
+    backgroundColor: '#22223b',
+    borderRadius: 10,
+    elevation: 1,
+    marginVertical: 10,
+  },
+  buttonRegister: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 15,
+    backgroundColor: '#4A4E69',
+    borderRadius: 10,
+    elevation: 1,
+    marginTop: 10,
   },
 })
