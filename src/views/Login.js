1f04de4d95611ed8140f923a2eda8d24df352b0a
luisxfgc <lipegbi01@gmail.com>
2022-10-03 17:32:54 -0300

    changes on login

--- src/views/Login.js
+++ src/views/Login.js
@@ -17,7 +17,7 @@ import {
 export default function Login({ navigation }) {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [isLoading, setIsLoading] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
   const [isPressed, setIsPressed] = useState(false)
 
   const handleSubmit = () => {
--- src/views/LoginNativeBase.js
+++ src/views/LoginNativeBase.js
@@ -1,59 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Image } from 'react-native'
+import { auth } from '../config/firebase'
 import {
   NativeBaseProvider,
-  Box,
   Center,
   FormControl,
   Stack,
   Input,
-  WarningOutlineIcon,
-  Heading,
   Text,
+  Button,
+  Alert,
+  Divider,
+  Heading,
+  VStack,
 } from 'native-base'
 
-export function Form() {
-  return (
-    <Box>
-      <Center height={'full'}>
-        <Box alignItems={'center'} my={'20'}>
-          <Heading>Criar uma Conta</Heading>
-          <Text>insira seus dados para criar sua conta no helpUS</Text>
-        </Box>
-        <FormControl isRequired>
-          <Stack mx="4" p="2">
-            <FormControl.Label>Email</FormControl.Label>
-            <Input type="email" placeholder="Digite seu email" />
-            <FormControl.ErrorMessage
-              color={'red.500'}
-              leftIcon={<WarningOutlineIcon size="xs" />}
-            >
-              Insira um endereço de email válido
-            </FormControl.ErrorMessage>
-          </Stack>
+export default function LoginNativeBase({ navigation }) {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const img = require('../assets/login_image.png')
 
-          <Stack mx="4" p="2">
-            <FormControl.Label>Senha</FormControl.Label>
-            <Input type="password" placeholder="Digite sua senha" />
-            <FormControl.HelperText>
-              Must be atleast 6 characters.
-            </FormControl.HelperText>
-            <FormControl.ErrorMessage
-              color={'red.500'}
-              leftIcon={<WarningOutlineIcon size="xs" />}
-            >
-              Senha incorreta.
-            </FormControl.ErrorMessage>
+  const handleSubmit = () => {
+    setIsLoading(true)
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        var user = userCredential.user
+        navigation.navigate('Dashboard')
+      })
+      .catch((error) => {
+        if (email === '' || password === '') {
+          Alert.alert(
+            'Please enter your email and password',
+            'error',
+            error.message
+          )
+        }
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }
+
+  return (
+    <NativeBaseProvider>
+      <Center maxW={'80%'} height={'full'} m={'10'}>
+        <Stack alignItems="center" mb="5" space={2}>
+          <Heading size="2xl" fontWeight="black">
+            Entrar
+          </Heading>
+          <Text>Insira seus dados para entrar ou crie uma conta.</Text>
+        </Stack>
+        <FormControl isRequired>
+          <Stack space={5}>
+            <Stack>
+              <FormControl.Label>Email</FormControl.Label>
+              <Input
+                type="email"
+                autoCapitalize="none"
+                p={2}
+                placeholder="Email"
+                onChangeText={(email) => setEmail(email)}
+              />
+            </Stack>
+            <Stack>
+              <FormControl.Label>Password</FormControl.Label>
+              <Input
+                type="password"
+                p={2}
+                placeholder="Password"
+                onChangeText={(password) => setPassword(password)}
+              />
+            </Stack>
+            <Stack space={2} mt="5">
+              <Button p={'3'} onPress={handleSubmit}>
+                Entrar
+              </Button>
+              <Divider alignSelf="center" w="90%" />
+              <Button p={'3'} onPress={() => navigation.push('Register')}>
+                Criar uma conta
+              </Button>
+            </Stack>
           </Stack>
         </FormControl>
       </Center>
-    </Box>
-  )
-}
-
-export default function LoginNativeBase() {
-  return (
-    <NativeBaseProvider>
-      <Form />
     </NativeBaseProvider>
   )
 }
